eee4bae91082e4664c59488342cbc883b40bc8d4
Abrar Ahmed <farheen.f@cognizant.com>
2020-04-14 20:20:20 +0530

    OAuth configuration

---
+++ platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package io.pivotal.pal.tracker.oauthserver;
+
+/**
+ * @author Farheen
+ *
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+@SpringBootApplication
+public class App extends AuthorizationServerConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
