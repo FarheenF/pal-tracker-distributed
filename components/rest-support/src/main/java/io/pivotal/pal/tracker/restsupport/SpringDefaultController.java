36c7a2cda165f85413053a39c826d4ffc7ca4d50
Abrar Ahmed <farheen.f@cognizant.com>
2020-04-23 23:20:11 +0530

    Updated config server properties

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/SpringDefaultController.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/SpringDefaultController.java
@@ -1,17 +1,13 @@
 package io.pivotal.pal.tracker.restsupport;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringDefaultController {
-	
-	@Value("${default.message}")
-	private String message;
 
     @GetMapping("/")
     public String defaultRoute() {
-        return message;
+        return "Noop!";
     }
 }
